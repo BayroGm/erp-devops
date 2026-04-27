deede38d83ad32fae4fed14eab0a0561c3e8f013
unknown <luisbayro1813@gmail.com>
2026-04-27 01:16:12 -0600

    ramas

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+let TOKEN ="snyk_uat.1fcad39e.eyJlIjoxNzg1MDQ2NzY3LCJoIjoic255ay5pbyIsImoiOiJBWjNObC1hVjJPNkF3azJCSmJjU3hnIiwicyI6IlI0eDlwSTZvUWk2YnA4NFNjVzlaYXciLCJ0aWQiOiJBQUFBQUFBQUFBQUFBQUFBQUFBQUFBIn0.MKj52aA4CzZuTzwn_x8l8KtJHBWQH6bnc7cnDy6surE3dHLUbb46N9Y1FvuDO8tteS_oMJil3ZTh7yeAqWpnAA"
 // Endpoint de Salud (Para que App Runner sepa que el ERP no ha tronado)s
 app.get('/health', (req, res) => {
     res.status(200).json({ 
